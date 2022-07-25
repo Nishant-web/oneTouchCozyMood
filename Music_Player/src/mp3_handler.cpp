44c8b553f90765e057e6849c3db2bcb9dfe463a4
Amit Rajendra Relan <98418741+amitrelan238@users.noreply.github.com>
2022-07-26 00:36:27 +0100

    deletion

--- Music_Player/src/mp3_handler.cpp
+++
@@ -1,3 +0,0 @@
-add_library(MP3_HANDLER STATIC mp3_handler.cpp)
-target_include_directories(MP3_HANDLER PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/Inc)
-target_link_libraries(MP3_HANDLER PUBLIC wiringPi mpg123 pthread ao)
